42767a9bf3c0eeecf01f026c33c934d8351a3ce5
Vasiliy Lupuleac <v_lupuleac@mail.ru>
2019-04-05 12:20:41 +0300

    Add dummy implementations of printer.py, test_printer.py

---
+++ task06/printer.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+from model import *
+
+
+class PrettyPrinter(ASTNodeVisitor):
+    def visit_number(self, num):
+        pass
+
+    def visit_func(self, func):
+        pass
+
+    def visit_func_def(self, func_def):
+        pass
+
+    def visit_cond(self, cond):
+        pass
+
+    def visit_print(self, pr):
+        pass
+
+    def visit_read(self, read):
+        pass
+
+    def visit_func_call(self, func_call):
+        pass
+
+    def visit_ref(self, ref):
+        pass
+
+    def visit_bin_op(self, bin_op):
+        pass
+
+    def visit_un_op(self, un_op):
+        pass
+
+
+def pretty_print(program):
+    pass
---
+++ task06/test_printer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import pytest
+from printer import *
+
+
+def test_dummy():
+    assert True
+
+
+if __name__ == "__main__":
+    pytest.main()
