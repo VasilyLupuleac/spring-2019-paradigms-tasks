1d62794c609a79e46de3eb0ab6a88d572912d2d4
Vasiliy Lupuleac <v_lupuleac@mail.ru>
2019-04-11 22:51:21 +0300

    Fix styling in test_printer.py

--- task06/test_printer.py
+++ task06/test_printer.py
@@ -10,22 +10,35 @@ def test_print_number():
     assert Number(1000).accept(pr) == "    1000;"
 
 
-def test_print_func_def():
+def test_print_func_def_empty():
     empty = FunctionDefinition("empty", Function([], []))
-    foo = FunctionDefinition("foo", Function(["a", "b", "c"], [Number(15)]))
     pr = PrettyPrinter()
     assert empty.accept(pr) == "def empty() {\n}"
+
+
+def test_print_func_def_indent():
+    foo = FunctionDefinition("foo", Function(["a", "b", "c"], [Number(15)]))
+    pr = PrettyPrinter()
     pr.depth = 1
     assert foo.accept(pr) == "    def foo(a, b, c) {\n        15;\n    }"
 
 
-def test_print_cond():
+def test_print_cond_empty():
     empty = Conditional(Number(42), [], [])
+    pr = PrettyPrinter()
+    assert empty.accept(pr) == "if (42) {\n}"
+
+
+def test_print_cond_indent():
     cond = Conditional(Number(0), [Number(1), Number(10)], [Number(-1)])
     pr = PrettyPrinter()
-    assert cond.accept(pr) == "if (0) {\n    1;\n    10;\n} else {\n    -1;\n}"
     pr.depth = 1
-    assert empty.accept(pr) == "    if (42) {\n    }"
+    assert cond.accept(pr) == """    if (0) {
+        1;
+        10;
+    } else {
+        -1;
+    }"""
 
 
 def test_print_print():
@@ -44,11 +57,15 @@ def test_print_read():
     assert read_x.accept(pr) == "    read x;"
 
 
-def test_print_func_call():
+def test_print_func_call_empty():
     empty = FunctionCall(Reference("empty"), [])
-    foo = FunctionCall(Reference("foo"), [Number(1), Number(2), Number(3)])
     pr = PrettyPrinter()
     assert empty.accept(pr) == "empty();"
+
+
+def test_print_func_call_indent():
+    foo = FunctionCall(Reference("foo"), [Number(1), Number(2), Number(3)])
+    pr = PrettyPrinter()
     pr.depth = 1
     assert foo.accept(pr) == "    foo(1, 2, 3);"
 
@@ -95,10 +112,18 @@ def test_end_to_end(capsys):
     ])))
     out, err = capsys.readouterr()
     assert not err
-    assert out == ("def main(arg1) {\n    read x;\n" + "    print x;\n" +
-                   "    if (((2) == (3))) {\n" + "        if (1) {\n" +
-                   "        }\n" + "    } else {\n" +
-                   "        exit((-(arg1)));\n" + "    }\n" + "}\n")
+    print(out)
+    assert out == """def main(arg1) {
+    read x;
+    print x;
+    if (((2) == (3))) {
+        if (1) {
+        }
+    } else {
+        exit((-(arg1)));
+    }
+}
+"""
 
 
 if __name__ == "__main__":
