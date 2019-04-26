18bded80a3679b1eb6d878be0d7761fb39084769
Vasiliy Lupuleac <v_lupuleac@mail.ru>
2019-04-27 00:23:18 +0300

    Fix tsqueue.cpp

--- task09/src/tsqueue.cpp
+++ task09/src/tsqueue.cpp
@@ -3,11 +3,13 @@
 void threadsafe_queue_init(ThreadsafeQueue *q) {
     queue_init(&(q->q));
     pthread_mutex_init(&(q->m), nullptr);
+    pthread_cond_init(&(q->cond), nullptr);
 }
 
 void threadsafe_queue_destroy(ThreadsafeQueue *q) {
     queue_destroy(&(q->q));
     pthread_mutex_destroy(&(q->m));
+    pthread_cond_destroy(&(q->cond));
 }
 
 void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
@@ -20,7 +22,7 @@ void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
 void *threadsafe_queue_wait_and_pop(ThreadsafeQueue *q) {
     pthread_mutex_lock(&(q->m));
     while (queue_empty(&(q->q)))
-        pthread_cond_wait(&(q->cond));
+        pthread_cond_wait(&(q->cond), &(q->m));
     void *data = queue_pop(&(q->q));
     pthread_mutex_unlock(&(q->m));
     return data;
